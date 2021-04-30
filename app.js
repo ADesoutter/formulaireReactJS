e1861ad13ceb15d36039b5f8395fe2cbd0bcc953
Turritopsis <alexandredesoutter.pro@gmail.com>
2021-04-30 17:26:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+    class App extends React.Component {
+
+        state = {
+            newName: "",
+            newComment: "",
+            history: []
+        }
+
+        changeName = (event) => {
+            this.setState({
+                newName: event.target.value,
+            })
+        }
+    
+        changeComment = (event) => {
+            this.setState({
+            newComment: event.target.value,
+            })
+        }
+    
+    
+        keepHistory = (event) => {
+            event.preventDefault();
+    
+        let allInformations = [...this.state.history, this.state.newName, this.state.newComment]
+    
+        this.setState({
+            history: allInformations
+        })
+    
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                <form className="form">
+                <label htmlFor="">Say Something</label>
+                    <div>  
+                        <input type="text" placeholder="Your Name" 
+                        onChange={this.changeName} value={this.state.newName}/>
+                    </div>
+                    <div>
+                        <textarea name="" id="" placeholder="Your comment"
+                        onChange={this.changeComment} value={this.state.newComment}></textarea>
+                    </div>
+                    <div>
+                        <input onClick={this.keepHistory} type="submit" value="Comment"/>
+                    </div>
+                </form>
+            </div>
+        )}
+    }
+
+    ReactDOM.render(<App />, document.getElementById('app'));
